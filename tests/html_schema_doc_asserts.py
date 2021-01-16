08f88a37477809a2c29f21581ee42a51b558ed16
Francois Chastanet <francois.chastanet@crossknowledge.com>
2021-01-16 01:02:24 +0100

    removed debug

--- tests/html_schema_doc_asserts.py
+++ tests/html_schema_doc_asserts.py
@@ -1,7 +1,6 @@
 from typing import List, Optional
 
 from bs4 import BeautifulSoup, Tag
-from pprint import pprint
 
 def assert_soup_results_text(soup: BeautifulSoup, class_name: str, texts: List[str]) -> None:
     """Assert that all the HTML elements with the provided class found in the schema has the supplied text
@@ -14,8 +13,6 @@ def assert_soup_results_text(soup: BeautifulSoup, class_name: str, texts: List[s
 
     for i, element in enumerate(elements):
         assert element.text.strip() == texts[i]
-        pprint(element.text.strip())
-
 
 def assert_property_names(soup: BeautifulSoup, property_names: List[str]) -> None:
     assert_soup_results_text(soup, "property-name", property_names)
