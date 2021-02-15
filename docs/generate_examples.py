7e3994fa278f4e68b999a50359848bef9ac60146
Francois Chastanet <fchastanet@gmail.com>
2021-02-15 22:46:43 +0100

    ignore docs/_includes/README.md

--- docs/generate_examples.py
+++ docs/generate_examples.py
@@ -14,25 +14,27 @@ if templateName == 'md':
     print("please use tests/generate_expected_md.py instead")
     sys.exit()
 
-sys.path.insert(0, os.path.abspath(".."))
+current_dir = os.path.dirname(__file__)
+parent_dir = os.path.dirname(current_dir)
+sys.path.insert(0, parent_dir)
 
 from json_schema_for_humans.generate import generate_from_filename, GenerationConfiguration
 
-cases_source_dir = os.path.abspath(os.path.join(os.path.dirname(os.getcwd()), "tests", "cases"))
-includes_dir = os.path.join(os.getcwd(), "_includes")
-json_examples_dir = os.path.join(os.getcwd(), "_includes", "examples")
+cases_source_dir = os.path.abspath(os.path.join(parent_dir, "tests", "cases"))
+includes_dir = os.path.join(current_dir, "_includes")
+json_examples_dir = os.path.join(current_dir, "_includes", "examples")
 os.makedirs(json_examples_dir, exist_ok=True)
-asserts_dir = os.path.join(os.getcwd(), "assets")
+asserts_dir = os.path.join(current_dir, "assets")
 results_example_dir = os.path.join(asserts_dir, f"examples_{templateName}")
 os.makedirs(results_example_dir, exist_ok=True)
 
 
 readme = "README.md"
-readme_location = os.path.abspath(os.path.join(os.path.dirname(os.getcwd()), readme))
+readme_location = os.path.abspath(os.path.join(parent_dir, readme))
 shutil.copyfile(readme_location, os.path.join(includes_dir, readme))
 
 config_schema = "config_schema.json"
-config_schema_location = os.path.abspath(os.path.join(os.path.dirname(os.getcwd()), config_schema))
+config_schema_location = os.path.abspath(os.path.join(parent_dir, config_schema))
 generate_from_filename(config_schema_location, os.path.join(asserts_dir, "config_schema.html"), expand_buttons=True)
 configMd = GenerationConfiguration(deprecated_from_description=True, template_name="md")
 generate_from_filename(config_schema_location, os.path.join(asserts_dir, "config_schema.md"), config=configMd)
