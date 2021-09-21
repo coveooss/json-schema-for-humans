de7d88f3359a2a70b7e8714466e3b40be960539b
Jonathan Piché <jpiche@coveo.com>
2021-09-21 11:08:28 -0400

    use a proper python tool install script

--- .github/workflows/actions/publish-to-pypi/get-minimum-version.sh
+++ .github/workflows/actions/publish-to-pypi/get-minimum-version.sh
@@ -1,4 +1,4 @@
 set -e
-PROJECT_NAME_AND_VERSION=$(python -m poetry version)
+PROJECT_NAME_AND_VERSION=$(poetry version)
 VERSION_ONLY=$(echo "$PROJECT_NAME_AND_VERSION" | cut --fields 2 --delimiter ' ')
 echo "$VERSION_ONLY"
